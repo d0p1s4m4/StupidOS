4e110d12eb20a22667d012015a8b5f1f27db0f5c
d0p1 <contact@d0p1.eu>
2023-06-06 16:03:35 +0200

    chore: add logo and reorganize docs

--- docs/build.mk
+++ docs/build.mk
@@ -1,5 +1,5 @@
 .PHONY: docs
 docs: 
 	-mkdir -p $(DOC_DIR)/html
-	naturaldocs -p $(DOC_DIR)/config -xi tmp -i . -o HTML $(DOC_DIR)/html
-	cp .github/favicon.ico $(DOC_DIR)/html/
+	naturaldocs -p $(DOC_DIR)/config -img $(DOC_DIR)/img -xi tmp -i . -o HTML $(DOC_DIR)/html
+	cp $(DOC_DIR)/img/favicon.ico $(DOC_DIR)/html/
