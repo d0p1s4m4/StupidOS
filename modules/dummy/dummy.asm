80aca5ee8a3c9c9a4c10bbb11140c0b11a345184
d0p1 <contact@d0p1.eu>
2024-07-08 19:20:24 +0200

    chore: kernel module ?

--- modules/dummy/dummy.asm
+++ modules/dummy/dummy.asm
@@ -1,2 +1,21 @@
 	format COFF
+	use32
 
+	include '../module.inc'
+
+	section '.text' code
+
+	public module_init
+module_init:
+	ret
+
+	public module_exit
+module_exit:
+	ret
+
+MODULE_INFO_BEGIN
+
+MODULE_AUTHOR "d0p1"
+MODULE_DESCRIPTION "Dummy module"
+MODULE_LICENSE "BSD-3-Clause"
+MODULE_VERSION "1.0"
