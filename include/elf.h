c84e5b55e407c8e7f15f42119cda27e46ae4d91f
d0p1 <contact@d0p1.eu>
2024-08-18 14:19:28 +0200

    chore: sync repo

--- include/elf.h
+++ include/elf.h
@@ -3,6 +3,21 @@
 
 # include <stdint.h>
 
+# define ELF_MAG0 0x7F
+# define ELF_MAG1 0x45
+# define ELF_MAG2 0x4C
+# define ELF_MAG3 0x46
+
+# define EI_NIDENT 16
+
+struct elf_header 
+{
+	uint8_t e_ident[EI_NIDENT];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+};
+
 /* TODO */
 
 #endif /* !ELF_H */
